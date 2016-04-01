3efbaa2c2af666b2d67445cb4b9c686a70dab551
mad-hu <450287549@qq.com>
2016-04-01 15:40:07 +0800

    修改部分特效

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         currentIndex = 0,
         interval,
         hasStarted = false,
-        t = 3000;
+        t = 5000;
     length = $('.ps-bg-item').length;
     $('.ps-bg-item:not(:first)').hide();
     $('.trigger-item:first').addClass('current');
@@ -39,20 +39,17 @@ $(document).ready(function () {
     }
 
     function play(preIndex,currentIndex){
-        $('.ps-bg-item').removeClass('init');
-        $('.ps-bg-item').eq(currentIndex).addClass('init');
-        $('.ps-bg-item').eq(preIndex).fadeOut(500).parent().children().eq(currentIndex).fadeIn(500);
-
-
+        $('.ps-bg-item').eq(preIndex).fadeOut(1000).parent().children().eq(currentIndex).fadeIn(1000);
         $('.ps-bg-item').eq(currentIndex).siblings().css({"z-index": "0"}),$('.ps-bg-item').eq(currentIndex).css({zIndex: "1"}).stop(!0).animate({opacity: "1"});
 
 
         var i = $('.ps-item').eq(currentIndex);
-        i.siblings().css({"z-index": "4"}), i.css({zIndex: "5"}).find(".puzzle-item.item-1 .puzzle-item-block").stop(!0).animate({"margin-left": "0px"});
-
-        //i.find(".puzzle-item.item-2 .puzzle-item-block").stop(!0).animate({"margin-left": "0px"},
-        //    $('.ps-item').speed - 200, "easeInOutQuint"), i.find(".puzzle-item.item-3 .puzzle-item-block").stop(!0).animate({"margin-left": "0px"},
-        //    $('.ps-item').speed - 100, "easeInOutQuint"), i.find(".puzzle-item.item-4 .puzzle-item-block").stop(!0).animate({"margin-left": "0px"});
+        i.siblings().css({"z-index": "4"}), i.css({zIndex: "5"}).find(".puzzle-item.item-1 .puzzle-item-block").stop(!0).animate({"margin-left": "0px"},function(){
+            var e = i;
+            e.siblings().each(function () {
+                $(this).css({"z-index": "4"}).removeClass("current").removeClass("init"), $(this).find(".puzzle-item").find(".puzzle-item-block").removeAttr("style")
+            })
+        });
 
         //鼠标划过后 圆圈是否被选中
         $('.trigger-item').eq(currentIndex).addClass("current").siblings().removeClass("current");
